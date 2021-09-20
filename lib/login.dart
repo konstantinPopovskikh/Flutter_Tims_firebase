9772c41ed310840eeea481a64043fb0b49c856b5
Konstantin <konstantin.popovskikh@smartforce.io>
2021-09-20 14:51:27 +0600

    added authentication

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hello_flutter/my_home_page.dart';
+import 'google_auth.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -23,35 +24,48 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  String name = '';
   TextEditingController controller = TextEditingController();
 
   void click() {
-    name = controller.text;
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => MyHomePage(name: name)));
+    FirebaseService().signInwithGoogle().then((value) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => MyHomePage(name: value.email.toString())));
+    });
   }
+
+  // Widget googleLoginButton() {
+  //   return OutlinedButton(
+  //     onPressed: click,
+  //     child: const Text('Sign in with google'),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.center,
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: TextField(
-          controller: controller,
-          decoration: InputDecoration(
-              prefixIcon: const Icon(Icons.person),
-              labelText: 'Type your name',
-              border:
-                  const OutlineInputBorder(borderSide: BorderSide(width: 5)),
-              suffixIcon: IconButton(
-                onPressed: click,
-                tooltip: 'Submit',
-                icon: const Icon(Icons.done),
-                splashColor: Colors.blue,
-              )),
-        ),
+    return Center(
+      child: Column(
+        children: [
+          ElevatedButton(
+            child: const Text(
+              'Sign in with Google',
+              style: TextStyle(fontSize: 20),
+            ),
+            onPressed: () {
+              click();
+            },
+          ),
+          ElevatedButton(
+            child: const Text(
+              'Sign out',
+              style: TextStyle(fontSize: 20),
+            ),
+            onPressed: () {
+              FirebaseService().signOutFromGoogle();
+            },
+          ),
+        ],
       ),
     );
   }
